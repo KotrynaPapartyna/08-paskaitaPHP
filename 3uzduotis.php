fb5f7b7822616849f976c1fd4142d279070626bc
KotrynaPapartyna <kmarkeviciute@gmail.com>
2021-08-01 20:57:27 +0300

    pradeta bet dar nebaigta

---
+++ 3uzduotis.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3uzduotis</title>
+</head>
+
+<style>
+    table, th, td {
+        border: 2px solid black;
+}
+
+</style>
+<body>
+
+<table style="width:100%">
+  <tr>
+    <th>Vardas</th>
+    <th>Pavarde</th>
+    <th>Asmens kodas</th>
+    <th>Prisijungimo data</th>
+    <th>Adresas</th>
+    <th>El. pastas</th>
+  </tr>
+
+<?php
+
+//3. Susikurti asociatyvų masyvą "Klientai".
+// Kintamieji: vardas, pavarde, asmens kodas, prisijungimo data, adresas,elpastas.
+
+///Masyve turi būti 200 klientų. Duomenis užpildyti savo nuožiūrą.
+///Visą "Klientai" objektų masyvą atvaizduoti lentelėje <table>.
+///Visas klientų masyvas saugomas COOKIE.
+
+$klienrai=array(
+    "vardas"=>"vardas1", 
+    "pavarde"=>"pavarde1", 
+    "asmensKodas"=>"1234567891", 
+    "prisijungimoData"=>"prisijungimoData1",
+    "adresas"=>"adresas1",
+    "elPastas"=>"elPastas1",
+
+)
+
+?>
+    
+</body>
+</html>
